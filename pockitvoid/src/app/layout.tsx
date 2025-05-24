c9998ea353495aabfd8860b21877b8532bd64de5
prnth <pranith_h@yahoo.com.sg>
2025-05-24 21:02:21 +0530

    cleanup

--- pockitvoid/src/app/layout.tsx
+++ pockitvoid/src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 };
 
 const allExperiments = [
-  'barebones', 'car', 'interior', 'thirdperson', 'thirdpersonwawa', 'thirdperson6jump', 'firstperson', 'npc', 'xr', 'miladychess', 'miladysurfer', 'playground',
+  'barebones', 'ground', 'lighting', 'car', 'interior', 'thirdpersonwawa', 'thirdperson6jump', 'thirdperson7animations', 'npc', 'xr', 'miladychess', 'miladysurfer', 'playground',
 ]
 
 
--- pockitvoid/src/app/lighting/page.tsx
+++ pockitvoid/src/app/lighting/page.tsx
@@ -3,21 +3,23 @@
 import { Canvas } from "@react-three/fiber";
 import { Physics, RigidBody } from "@react-three/rapier";
 import { OrbitControls } from "@react-three/drei";
+import Lightsource from "./lightsource";
 
 export default function Home() {
     return (
         <div className="items-center justify-items-center min-h-screen">
             <div className="w-full" style={{ height: "100vh" }}>
-                <Canvas>
+                <Canvas shadows>
                     <Physics>
+                        <Lightsource model="/models/environment/lamppost2.glb" position={[-1, -2, 1]} />
                         <RigidBody>
-                            <mesh>
+                            <mesh castShadow receiveShadow>
                                 <boxGeometry args={[1, 1, 1]} />
                                 <meshStandardMaterial color="orange" />
                             </mesh>
                         </RigidBody>
                         <RigidBody type="fixed">
-                            <mesh position={[0, -2, 0]} scale={[10, 0.5, 10]}>
+                            <mesh position={[0, -2, 0]} scale={[10, 0.5, 10]} receiveShadow>
                                 <boxGeometry />
                                 <meshStandardMaterial color="gray" />
                             </mesh>
